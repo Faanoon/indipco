dd15aec37d924af5a88b9bdab031ecc9f1e8d9d2
santoshbb <santosh.baburao@gmail.com>
2019-10-23 22:26:06 +0300

    t

--- indipco/config/desktop.py
+++ indipco/config/desktop.py
@@ -6,9 +6,9 @@ def get_data():
 	return [
 		{
 			"module_name": "indipco",
-			"color": "red",
+			"color": "blue",
 			"icon": "octicon octicon-file-directory",
 			"type": "module",
-			"label": _("indipco")
+			"label": _("indipco") 
 		}
 	]
