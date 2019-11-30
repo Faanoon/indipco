78f81ba33a7494abab1da43f4f73510d096dd43e
santoshbb <santosh.baburao@gmail.com>
2019-11-30 18:47:41 +0300

    Salary Slip-validation added to Leave without Pay 25 percent

--- indipco/hooks_call/salary_slip.py
+++ indipco/hooks_call/salary_slip.py
@@ -7,6 +7,9 @@ from datetime import timedelta
 
 #@frappe.whitelist(allow_guest=True)
 def validate_calculate_end_date(self,method):
-    self.total_working_days=30
+#    self.total_working_days=30
+#    self.payment_days=self.total_working_days-self.leave_without_pay
+    if (self.ind_leave_without_pay_25_percent>self.leave_without_pay):
+        frappe.throw(_("Leave without Pay 25 percent should be less than Leave Without Pay"))
 #    self.end_date=self.start_date+datetime.timedelta(days=30)
 #            frappe.throw(_("test"))
