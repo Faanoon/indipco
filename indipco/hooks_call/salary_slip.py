7c698d709d8ee36fd8153619babe99082ec52aed
santoshbb <santosh.baburao@gmail.com>
2020-01-15 20:37:37 +0300

    Salary Slip,removed condition for 30 days

--- indipco/hooks_call/salary_slip.py
+++ indipco/hooks_call/salary_slip.py
@@ -12,5 +12,5 @@ def validate_calculate_end_date(self,method):
     if ((self.ind_leave_without_pay_25_percent + self.ind_sl100) > self.leave_without_pay):
         frappe.throw(_("Sick Leave 100 pay and 75 pay should be less than Total Leave"))
 
-    if (self.payroll_frequency=="Monthly" and self.total_working_days!=30):
-         frappe.throw(_("Working Days should be 30, Please change End Date"))
+#    if (self.payroll_frequency=="Monthly" and self.total_working_days!=30):
+#         frappe.throw(_("Working Days should be 30, Please change End Date"))
