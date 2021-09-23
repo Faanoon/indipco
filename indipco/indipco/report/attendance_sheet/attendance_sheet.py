ea320ef401b5d105db2b2153bacfa2ab19c7ee10
root <root@erp.qimc.com.qa>
2021-09-24 00:45:48 +0300

    Attendance Sheet customized

--- indipco/indipco/report/attendance_sheet/attendance_sheet.py
+++ indipco/indipco/report/attendance_sheet/attendance_sheet.py
@@ -14,12 +14,14 @@ def execute(filters=None):
 	
 def get_columns():
 	return [
-        _("Attendance") + ":Link/Attendance:180",
+#        _("Attendance") + ":Link/Attendance:180",
         _("Employee") + ":Link/Employee:100",
 #        _("Employee Name") + ":Data:1",
 #        _("Attendance Date") + ":Data:140",
         _("Status") + ":Data:140",
-        _("Total Days") + ":Int:140",
+#        _("Total Absent Days") + ":Int:140",
+#        _("Total Present Days") + ":Int:160",
+        _("Total Working Days") + ":Int:160",
         _("Working Hours") + ":Data:140",
         _("Deductible Hours") + ":Data:140"
 	]
@@ -27,16 +29,19 @@ def get_columns():
 def get_data(filters):
 	from_date=filters.get("from_date")
 	to_date=filters.get("to_date")
+	employee=filters.get("employee")
 	return frappe.db.sql("""
 		select
-		A.name,
+#		A.name,
 		A.employee,
 #        A.employee_name,
 #        A.attendance_date,
         A.status,
-        A.status,
-		A.working_hours,
-        A.ind_deductible_hours
+#        sum(if(A.status="Absent",1,0)),
+#        sum(if(A.status!="Absent",1,0)),
+        sum(if(A.status="Absent",1,0))+sum(if(A.status!="Absent",1,0)),
+		sum(A.working_hours),
+        sum(A.ind_deductible_hours)
 					
 		FROM
 		`tabAttendance` as A
@@ -44,6 +49,7 @@ def get_data(filters):
 		WHERE
 		A.attendance_date>='%s'
 		&&A.attendance_date<='%s'
+		&&A.employee='%s'
         
-		GROUP BY A.employee
-		ORDER BY A.employee ASC """ %(from_date,to_date), as_list=1)
+		GROUP BY A.status
+		ORDER BY A.status ASC """ %(from_date,to_date,employee), as_list=1)
